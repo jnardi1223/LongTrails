6b7aaa3f159ec140c6623d7b64d0f93913cadcc7
Jim <jnardi1223@gmail.com>
2021-09-29 20:17:40 -0400

    auth styled except for main logo

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -15,12 +15,17 @@ import { Link } from "react-router-dom";
 
 
 const logo = (
-        <div className="logo-title">
+        <div className="nav-logo-title">
              <Link to="/">
                 <button>Long Trails</button>
              </Link>
         </div> )
 
+const navSiteLinks = (
+    <div className="nav-site-links">
+
+    </div>
+)
 
 const Nav = ({ user, signOut }) => {
     if (user) {
@@ -30,16 +35,17 @@ const Nav = ({ user, signOut }) => {
                 <div>
                     <p>Hello, {user.first_name}</p>
                 </div>
-                <button onClick={signOut}>Sign Out</button>
+                <button className="user-button" onClick={signOut}>Sign Out</button>
             </div>
         )
     } else {
         return (
             <div className="main-nav">
+                {navSiteLinks}
                 {logo}
                 <div className="user-auth">
-                    <Link className="button" to="/signup">Sign Up</Link>
-                    <Link className="button" to="/signin">Sign In</Link>
+                    <Link className="user-button" to="/signup">Sign Up</Link>
+                    <Link className="user-button" to="/signin">Sign In</Link>
                 </div>
             </div>
         )
