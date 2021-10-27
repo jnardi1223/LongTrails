063ec1a58e50f9e50515a48a1c1f83e5b0a712d3
jnardi1223 <77065234+jnardi1223@users.noreply.github.com>
2021-10-26 20:19:05 -0400

    reviews stars and dates now showing

--- frontend/components/reviews/review_item.jsx
+++ frontend/components/reviews/review_item.jsx
@@ -1,10 +1,18 @@
 import React from "react"; 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMountain, faHiking } from "@fortawesome/free-solid-svg-icons";
-import ReactStars from "react-rating-stars-component"
+import ReactStars from 'react-stars'
 
 const ReviewItem = ({review}) => {
     
+    function formatDate(date) {
+        const monthNames = ["January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"];
+
+        let splitDate = date.split("-");
+        return `${monthNames[parseInt(splitDate[1]) - 1]} ${splitDate[2]}, ${splitDate[0]}`;
+    }
+
     return (
         <div className="review-item">
             <div className="review-pic-name">
@@ -16,7 +24,8 @@ const ReviewItem = ({review}) => {
                         {review.reviewer.first_name} {review.reviewer.last_name}
                     </p>
                     <div className="star-date">
-                        
+                        <ReactStars value={review.rating}/>
+                        <p className="date">{formatDate(review.post_date)}</p>
                     </div>
                 </div>
             </div>
